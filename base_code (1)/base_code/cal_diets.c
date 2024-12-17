d90a8da40adcef7460a40f2bc050934e6c574857
MINSEO-0 <minseo104@sookmyung.ac.kr>
2024-12-18 06:19:07 +0900

    SMHealthcare

--- base_code (1)/base_code/cal_diets.c
+++ base_code (1)/base_code/cal_diets.c
@@ -68,9 +68,10 @@ void inputDiet(HealthData* health_data)
     for (i=0; i < diet_list_size; i++)
      // Run iterative loops to process all data sequentially. Processes from i=0 until less than diet_list_size - Minseo/ 2024.12.11
     {
-    	printf("%d. %s (%d kcal)\n", i, diet_list[i].food_name, diet_list[i].calories_intake);
+    	printf("%d. %s (%d kcal)\n", i + 1 , diet_list[i].food_name, diet_list[i].calories_intake);
     	 // Output of data for array index "(integer - List number, string - foodname, integer - calories information for current food) " Output in form - Minseo/ 2024.12.11
     	 // For statement i is a variable representing the number of the list, so write i rather than i+1 - Minseo/ 2024.12.11
+    	 // 
 	}
 	
 	printf("0. Exit\n"); // "0. Exit" Create exit options by printing exit options. - Minseo/ 2024.12.11
