1d2cf3e9527e8e8cc432b7b6916a4a4b8e627e10
MINSEO-0 <minseo104@sookmyung.ac.kr>
2024-12-18 04:43:53 +0900

    Update cal_healthdata.c

--- base_code (1)/base_code/cal_healthdata.c
+++ base_code (1)/base_code/cal_healthdata.c
@@ -121,6 +121,8 @@ void printHealthData(const HealthData* health_data) {
 	 // Displays basal metabolic rate on screen in integer format - Minseo / 2024.12.18
 	printf("Total calories burned: %d kcal\n", health_data->total_calories_burned);
 	 // Displays the total calories burned variable in the health_data structure on the screen in integer form - Minseo / 2024.12.18
+	printf("Total calories intake: %d kcal\n", health_data->total_calories_intake);
+	 // Displays the total calories intake variable in the health_data structure on the screen in integer form - Minseo / 2024.12.18 
  
  
     printf("=======================================================================\n \n");
