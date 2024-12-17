57d59d6cd9e8a203f90acb96c3cf912249f0e6d1
MINSEO-0 <minseo104@sookmyung.ac.kr>
2024-12-18 03:37:34 +0900

    Update cal_healthdata.c

--- base_code (1)/base_code/cal_healthdata.c
+++ base_code (1)/base_code/cal_healthdata.c
@@ -39,6 +39,8 @@ void saveData(const char* HEALTHFILEPATH, const HealthData* health_data)
     for(i = 0; i < health_data->exercise_count; i++)
      // Run iterative loops to process all data sequentially. Processes from i=0 until less than health_data->exercise_count(number of exercise) - Minseo / 2024.12.15
     {
+    	fprintf(file, "%s - %d kcal\n", health_data->exercises[i].exercise_name, health_data->exercises[i].calories_burned_per_minute);
+		 //Retrieves the exercise name from the exercise array in the health_data structure and the calories burned per minute information from the exercise array in the health_data structure and displays them on the file. - Minseo / 2024.12.18 
 	}
     
     // ToCode: to save the chosen diet and total calories intake 
