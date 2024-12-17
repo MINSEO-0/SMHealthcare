824c148fe3bf59bda5eff93a8e24bafccac7b945
MINSEO-0 <minseo104@sookmyung.ac.kr>
2024-12-18 06:10:12 +0900

    Update main.c

--- base_code (1)/base_code/main.c
+++ base_code (1)/base_code/main.c
@@ -83,7 +83,9 @@ int main()
                 break;
                 
             case 4:
-            	 //The data entered by the user must be saved before ending the program - Minseo / 2024.12.18 
+            	 //The data entered by the user must be saved before ending the program - Minseo / 2024.12.18
+            	saveData(HEALTHFILEPATH, &health_data);
+				 // Save entered data such as diet and exercise to a file - Minseo / 2024.12.18
     			printf("Exit the system.\n");
     			printf("=======================================================================\n");
                 break;
